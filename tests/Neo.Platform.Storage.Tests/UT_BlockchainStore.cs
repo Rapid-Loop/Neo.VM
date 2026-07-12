55b0377eecd5a7cd62f37d9a0d89dd79a6842d82
Christopher Schuchardt <8141309+cschuchardt88@users.noreply.github.com>
2026-07-12 00:36:44 -0400

    Added Seek Tests

--- tests/Neo.Platform.Storage.Tests/UT_BlockchainStore.cs
+++ tests/Neo.Platform.Storage.Tests/UT_BlockchainStore.cs
@@ -20,6 +20,8 @@
 // DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
 // SERVICES
 
+using System.Linq;
+
 namespace Neo.Platform.Storage.Tests
 {
     [TestClass]
@@ -43,6 +45,31 @@ namespace Neo.Platform.Storage.Tests
             Assert.IsNotNull(actualBytes);
             Assert.AreEqual(1, actualBytes?.Length);
             Assert.AreEqual<byte?>(0x01, actualBytes?[0]);
+
+            TestStorePool.Shared.Return(store);
+        }
+
+        [TestMethod]
+        public void TestSnapshotColumnFamilyNames()
+        {
+            var store = TestStorePool.Shared.Rent();
+
+            store.Put([0xff, 0x00, 0x00], [0x00]);
+            store.Put([0xff, 0x00, 0x01], [0x01], ColumnFamilyNames.Blocks);
+
+            var actualBytes = store.Get([0xff, 0x00, 0x01]);
+
+            Assert.IsFalse(store.ContainsKey([0xff, 0x00, 0x01]));
+            Assert.IsNull(actualBytes);
+
+            using (var snapshot = store.CreateSnapshot())
+            {
+                actualBytes = snapshot.Get([0xff, 0x00, 0x01], ColumnFamilyNames.Blocks);
+
+                Assert.IsNotNull(actualBytes);
+                Assert.AreEqual(1, actualBytes?.Length);
+                Assert.AreEqual<byte?>(0x01, actualBytes?[0]);
+            }
 
             TestStorePool.Shared.Return(store);
         }
@@ -80,6 +107,36 @@ namespace Neo.Platform.Storage.Tests
                 Assert.AreEqual(1, actualBytes?.Length);
                 Assert.AreEqual<byte?>(0x01, actualBytes?[0]);
             }
+
+            TestStorePool.Shared.Return(store);
+        }
+
+        [TestMethod]
+        public void TestSeek()
+        {
+            var store = TestStorePool.Shared.Rent();
+
+            store.Put([0x00, 0x00, 0x00], [0x00]);
+            store.Put([0x00, 0x00, 0x01], [0x01]);
+            store.Put([0x00, 0x00, 0x02], [0x02]);
+            store.Put([0x00, 0x00, 0x03], [0x03]);
+            store.Put([0x00, 0x00, 0x04], [0x04]);
+
+            var actualResults = store.Seek((byte[])[0x00, 0x00, 0x01]).ToArray();
+
+            Assert.HasCount(4, actualResults);
+            CollectionAssert.AreEqual((byte[])[0x00, 0x00, 0x01], actualResults[0].Key);
+            CollectionAssert.AreEqual((byte[])[0x00, 0x00, 0x02], actualResults[1].Key);
+            CollectionAssert.AreEqual((byte[])[0x00, 0x00, 0x03], actualResults[2].Key);
+            CollectionAssert.AreEqual((byte[])[0x00, 0x00, 0x04], actualResults[3].Key);
+
+            actualResults = [.. store.Seek((byte[])[0x00, 0x00, 0x03], seekFromEnd: true)];
+
+            Assert.HasCount(4, actualResults);
+            CollectionAssert.AreEqual((byte[])[0x00, 0x00, 0x03], actualResults[0].Key);
+            CollectionAssert.AreEqual((byte[])[0x00, 0x00, 0x02], actualResults[1].Key);
+            CollectionAssert.AreEqual((byte[])[0x00, 0x00, 0x01], actualResults[2].Key);
+            CollectionAssert.AreEqual((byte[])[0x00, 0x00, 0x00], actualResults[3].Key);
 
             TestStorePool.Shared.Return(store);
         }
